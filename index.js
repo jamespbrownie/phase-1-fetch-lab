f23039ec92a3b717eeea83dc79e5b47396ffecac
James Brownie <jamespbrownie@gmail.com>
2021-09-30 19:20:42 -0500

    commit tree 665e9a3f13fe1302e8dcc071a75004d6ee58a225

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json()).then(bookData => renderBooks(bookData))
 }
 
 function renderBooks(books) {
